b527dfc221b963f87380d7a0b2f9226576742ad3
Alex Vido <alexvido@pop-os.localdomain>
2023-04-17 17:14:17 -0300

    Feito exercício 11

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-  const body = document.querySelector('body');
   const ol = document.getElementById('lista-tarefas');
   const btnAdd = document.getElementById('criar-tarefa');
   const input = document.getElementById('texto-tarefa');
   const lis = document.getElementsByTagName('li');
-  const btnRemove = document.getElementById('apaga-tudo')
+  const btnRemove = document.getElementById('apaga-tudo');
+  const btnRemoveCompleted = document.getElementById('remover-finalizados');
 
   const changeColor = (e) => {
-    for (li of lis) {
-      li.style.backgroundColor = 'white';
+    for (let i = 0; i < lis.length; i += 1) {
+      lis[i].style.backgroundColor = 'white';
     }
     e.target.style.backgroundColor = 'gray';
   }
@@ -19,25 +19,34 @@
     } else {
       e.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)'
       e.target.classList.add('completed')
+      const concluded = document.getElementsByClassName('completed');
     }
   }
-
   const addList = () => {
     const li = document.createElement('li');
     li.innerText = input.value;
     li.addEventListener('click', changeColor);
     li.addEventListener('dblclick', scratch)
-    ol.appendChild(li)
+    ol.appendChild(li);
     input.value = '';
   }
 
   const cleanList = () => {
-    const lisOriginal = [...lis]
-    for (li of lisOriginal) {
-      ol.removeChild(li)
+    for (let i = lis.length - 1; i >= 0; i -= 1) {
+      ol.removeChild(lis[i]);
     }
   }
 
   btnAdd.addEventListener('click', addList);
   btnRemove.addEventListener('click', cleanList);
+
+
+  const remove = () => {
+    const concluded = document.getElementsByClassName('completed');
+    for (let i = concluded.length - 1; i >= 0; i -= 1) {
+      concluded[i].remove();
+    }
+  }
+
+  btnRemoveCompleted.addEventListener('click', remove);
 
